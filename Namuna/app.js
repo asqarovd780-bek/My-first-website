178d776f211afadd690a0123f1b899c693c21ff7
asqarovd780-bek <asqarovd780@gmail.com>
2025-10-30 20:47:53 +0500

    Add files via upload

---
+++ Namuna/app.js
@@ -0,0 +1,6 @@
+// script.js fayli
+
+// Tugmani bosganda sodir bo'ladigan funksiya
+document.getElementById('qabul_button').onclick = function() {
+    alert("Rahmat! Ro‘yxatdan o‘tish uchun qo‘ng‘iroq qiling yoki bizning menejer siz bilan bog‘lanadi.");
+};
